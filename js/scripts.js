1fd7e87f17a7511d1a19275eaaf855d33934a750
Andrew Peterson and Duncan Robbins <andrew@>
2019-08-14 13:27:04 -0700

    adds functional buttons for calc

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,48 @@
+
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter a number to add:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
 
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number to subtract:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
-
 var divide = function(number1, number2) {
   return number1 / number2;
 };
-
-var number1 = parseInt(prompt("Enter a number to devide:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number to multiply:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("#subBtn").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($(add1).val());
+    var number2 = parseInt($(add2).val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("#addBtn").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($(add1).val());
+    var number2 = parseInt($(add2).val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("#divBtn").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($(add1).val());
+    var number2 = parseInt($(add2).val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+  $("#mulBtn").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($(add1).val());
+    var number2 = parseInt($(add2).val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+});
